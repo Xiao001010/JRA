391b52c392f951550f6e2c13b387f33f0fe18860
Xiao001010 <haoxu.edu@outlook.com>
2023-08-07 17:40:59 +0100

    dataset definition

---
+++ dataset.py
@@ -0,0 +1,34 @@
+import os
+import tifffile
+
+import numpy as np
+
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
+# Define Dataset
+class CellDataset(Dataset):
+    def __init__(self, data_path, transform=None, transform_mask=None):
+        self.transform = transform
+        self.transform_mask = transform_mask
+        self.data_path = data_path
+        self.data = os.listdir(data_path)
+        self.data = [i for i in self.data if not i.endswith('_mask.tif')]
+        self.data.sort()
+        self.data = [os.path.join(data_path, i) for i in self.data]
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        img = tifffile.imread(self.data[idx])
+        img = img.astype(np.float32)
+        # print(img[0][0])
+        img = np.transpose(img, (1, 2, 0))
+        mask = tifffile.imread(self.data[idx][:-4] + '_mask.tif')
+        # print(mask)
+        if self.transform:
+            img = self.transform(img)
+            mask = self.transform_mask(mask)
+        return img, mask
+    
